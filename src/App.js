c018c18d2b5bea812222fd7cd2e7d5067a26c7ea
ayahdweikat <dweikatayah99@gmail.com>
2022-10-09 23:30:20 +0300

    Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Component/Navbar/Navbar';
+import Header from './Component/Header/Header';
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Header/>
     </div>
   );
 }
