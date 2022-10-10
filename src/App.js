97d2e5dcd5458f1c8c63395a56fa8b03415066b3
ayahdweikat <dweikatayah99@gmail.com>
2022-10-10 21:54:24 +0300

    Portfolio

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './Component/Navbar/Navbar';
 import Header from './Component/Header/Header';
+import Portfolio from './Component/Portfolio/Portfolio';
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Header/>
+      <Portfolio/>
     </div>
   );
 }
